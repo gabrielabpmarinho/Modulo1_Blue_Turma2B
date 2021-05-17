590aeb13079d042abad8044d1a59f8017bb9cd85
GABRIELA BARRETO PAIVA MARINHO <gvbpaiva@gmail.com>
2021-05-17 15:40:50 -0300

    Delete .vscode directory

--- .vscode/AULA 6/ex5.py
+++
@@ -1,9 +0,0 @@
-# 5.	Faça um programa que calcule através de uma função o IMC de uma pessoa que tenha 1,68 e pese 75kg.
-
-def imc():
-    peso = 1.68
-    altura = 75
-    return peso / altura
-
-
-print("Seu imc é",imc())
